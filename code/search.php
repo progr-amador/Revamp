0bd8b64f7a6e99c388400f3d13a6c8428a13cfe9
Alexandre Ramos <132162311+progr-amador@users.noreply.github.com>
2024-05-07 19:28:07 +0100

    filters partially working
    can't select more than 2 conditions (new and used for example)
    after every search the filters are cleared and have to input them again

--- code/search.php
+++ code/search.php
@@ -3,6 +3,7 @@
 
   require_once('../database/connection.db.php');
   require_once('../database/product.class.php');
+  require_once('../database/category.class.php');
 
   require_once('../templates/common.tpl.php');
   require_once('../templates/search.tpl.php');
@@ -11,14 +12,32 @@
 
   $query = $_GET['query'];
 
-  $searched = Product::getSearched($db, $query);
+  $categories = Brand::getCategories($db);
+  $districts = Brand::getDistricts($db);
+  $brands = Brand::getBrands($db);
 
   $name = "Search";
 
-  
+  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $title = $_GET['title'] ?? $query;
+    $price_min = $_GET["price_min"] ?? "";
+    $price_max = $_GET["price_max"] ?? "";
+    $condition = $_GET["condition"] ?? "";
+    $district = $_GET["district"] ?? "";
+    $category = $_GET["category"] ?? "";
+    $brand = $_GET["brand"] ?? "";
+    
+
+    // Call the getFiltered function with the form values
+    $searched = Product::getFiltered($db, $title, $price_min, $price_max, $condition, $district, $category, $brand);
+
+    // Process the filtered products, display them, etc.
+  }
 
   drawHead($name);
   drawHeader();
-  drawSearch($searched);
+  drawSearch($searched, $categories, $districts, $brands, $query);
   drawFooter();
 ?>
+
+
